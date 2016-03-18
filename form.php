32eac75e944a6c5a33bc3b761fe523b50dcdf086
Boris Todorov <1boris.todorov9@gmail.com>
2016-03-18 12:07:10 +0100

    store information in data.txt

--- form.php
+++ form.php
@@ -12,10 +12,13 @@
      * ako imamo post request onda mozemo da pocnemo normalizaciju i validaciju podataka koji nam dolaze,
      * preko forme
      */
-       //funkcija trim() skracuje string za nepotrebne intervale '    boris    ', 'boris'
+       //funkcija trim() skracuje string za nepotrebne intervale i simbole '    boris    ', 'boris'
        $username = trim($_POST['username']);
        $phone    = trim($_POST['phone']);
        $selectedGroup = (int)$_POST['groupe'];
+       
+       //deklarisemo promenljivu $error i dajemo vrednost false, nakon toga u if konstrukciji ako imamo neku gresku dajemo vrednost true
+       $error= FALSE;
        
        //Validacija podataka 
        /*
@@ -25,9 +28,11 @@
         */
        if (mb_strlen($username) <4){
            echo '<p>Pogresno ime</p>';
+           $error=TRUE;
        }
        if(mb_strlen($phone)<4 || mb_strlen($phone)>12){
            echo '<p>Pogresan broj</p>';
+           $error=TRUE;
        }
        
        /*
@@ -38,7 +43,21 @@
         */
        if(!array_key_exists($selectedGroup, $groups)){
            echo '<p>Pogresna grupa</p>';
+           $error=TRUE;
        }
+       
+       /*
+        * Podaci koji su uspesno prosli normalizaciju i validaciju zapisujemo u tekstualni fajl,
+        * Ako promenljiva $error u if konstrukciji daje vrednost true onda se pokazuju poruke za greske u mi 
+        * ne mozemo da zapisemo te podatke, zato opet okrecemo logiku ako promenljiva $error daje vrednost false 
+        * onda mozemo da krenemo sa zapisom podataka!
+        */
+       if (!$error){
+           
+           $results = $username.'!'.$phone.'!'.$selectedGroup;
+           echo $results;
+       }
+       
        
        
    }
